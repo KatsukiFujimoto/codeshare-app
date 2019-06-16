5debea7e44c01c17e1ee2f5186985aab27ad0e23
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-16 18:53:45 +0900

    Implement facebook authentication function

--- passport.js
+++ passport.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser(function(user, done) {
   done(null, user._id);
@@ -30,5 +33,40 @@ passport.use(new LocalStrategy({
 
       return done(null, user);
     })
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+  }, function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if(err) return done(err);
+
+      if(user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function(err, user) {
+          if(user) {
+            user.facebookId = profile.id
+            return user.save(function(err) {
+              if(err) return done(null, false, { message: "Can't save user info" });
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.id
+          user.save(function(err) {
+            if(err) return done(null, false, { message: "Can't save use info" });
+            return done(null, user);
+          });
+        })
+      }
+    });
   }
 ));
