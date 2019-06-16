07d345d5f6784002ac0f6c0f53fbd0e43d6af50e
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-16 14:59:10 +0900

    Set up the nodejs application

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Code4Share - a platform for sharing code.' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Code4Share - a platform for sharing code.' })
+});
+
+module.exports = router;
