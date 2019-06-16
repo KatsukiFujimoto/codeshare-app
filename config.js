7354ee0f150e17a4bdba617a73b8e41df259b454
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-17 02:07:01 +0900

    Prepare for deployment with mLab database

--- config.js
+++ config.js
@@ -12,6 +12,6 @@ module.exports = {
       pass: process.env.MY_GMAIL_PASSWORD
     }
   },
-  dbConnstring: 'mongodb://127.0.0.1:27017/codeshare',
+  dbConnstring: "mongodb+srv://" + process.env.MLAB_USERNAME + ":" + process.env.MLAB_PASSWORD + "@code4share-ubtb6.mongodb.net/test?retryWrites=true&w=majority",
   sessionKey: 'HaloCode4Share'
 }
